1098f6d695eb73a8600a003bb485db48ce49cf5b
tmokmss <tomookam@live.jp>
2023-05-18 16:15:13 +0900

    add the policy validator

--- cdk/bin/serverless-fullstack-webapp-starter-kit.ts
+++ cdk/bin/serverless-fullstack-webapp-starter-kit.ts
@@ -2,8 +2,11 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ServerlessFullstackWebappStarterKitStack } from '../lib/serverless-fullstack-webapp-starter-kit-stack';
+import { CfnGuardValidator } from '@cdklabs/cdk-validator-cfnguard';
 
-const app = new cdk.App();
+const app = new cdk.App({
+  policyValidationBeta1: [new CfnGuardValidator()],
+});
 new ServerlessFullstackWebappStarterKitStack(app, 'ServerlessFullstackWebappStarterKitStack', {});
 
 // import { Aspects } from 'aws-cdk-lib';
