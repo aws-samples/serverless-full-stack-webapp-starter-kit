8ed4f8014ff86cc4b5546a330ece77a5f783bc82
Masashi Tomooka <mtomooka@amazon.co.jp>
2025-04-17 17:33:08 +0900

    Update handler.mjs

--- cdk/lib/constructs/event-bus/handler.mjs
+++ cdk/lib/constructs/event-bus/handler.mjs
@@ -1,5 +1,11 @@
 import { util } from '@aws-appsync/utils';
 
+/**
+ * Allow subscription only for the channels that
+ * 1. begin with /public
+ * 2. begin with /user/<userId>
+ * https://docs.aws.amazon.com/appsync/latest/eventapi/channel-namespace-handlers.html
+ */
 export function onSubscribe(ctx) {
   if (ctx.info.channel.path.startsWith(`/event-bus/public`)) {
     return;
