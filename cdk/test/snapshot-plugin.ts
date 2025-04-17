398d22ecb4019442ba3537c5c32babba60f5adb3
Masashi Tomooka <mtomooka@amazon.co.jp>
2025-04-17 17:36:02 +0900

    fix

--- cdk/test/snapshot-plugin.ts
+++ cdk/test/snapshot-plugin.ts
@@ -3,6 +3,7 @@ module.exports = {
   serialize: (val: string) => {
     return `"${val //
       .replace(/([A-Fa-f0-9]{64}.zip)/, 'REDACTED')
+      .replace(/([A-Fa-f0-9]{64}.mjs)/, 'REDACTED')
       .replace(/.*cdk-hnb659fds-container-assets-.*/, 'REDACTED')
       .replace(/webapp-starter-[0-9a-z]*/, 'REDACTED')
       .replace(/(.*CurrentVersion).*/, '$1REDACTED')}"`;
